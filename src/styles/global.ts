1fa44ccad1b1705bac688b6a6905656bc517f547
Eduardo Marra <eduardomarra@gmail.com>
2022-06-11 19:10:31 -0300

    initial project setup

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle, css } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+    ${({ theme }) => css`
+        html {
+            font-size: 62.5%;
+            scroll-behavior: smooth;
+        }
+        body {
+            background-color: ${theme.colors.dark};
+        }
+        button {
+            cursor: pointer;
+            background: none;
+            border: none;
+        }
+        ul {
+            list-style: none;
+        }
+        a {
+            text-decoration: none;
+        }
+    `}
+`
+export default GlobalStyles
---
+++ src/styles/theme.ts
@@ -0,0 +1,37 @@
+export default {
+    grid: {
+        container: '108rem'
+    },
+    colors: {
+        primary: '#00D2DF',
+        light: '#ffffff',
+        grayLight: '#c4c4c4',
+        dark: '#121212',
+        darken: '#151515'
+    },
+    gradientColors: {
+        pink: 'linear-gradient(270.06deg, #6518B4 0%, #D24074 101.62%)',
+        purple: 'linear-gradient(269.96deg, #10D7E2 0%, #9358F7 100%)',
+        dark: 'linear-gradient(#121212, #121212)',
+        darken: 'linear-gradient(#151515, #151515)'
+    },
+    font: {
+        family: 'Inter, sans-serif',
+        sizes: {
+            small: '1.4rem',
+            medium: '1.6rem',
+            large: '1.8rem',
+            xlarge: '2.4rem',
+            xxlarge: '3.2rem',
+            huge: '6.4rem'
+        }
+    },
+    spacings: {
+        xsmall: '1.6rem',
+        small: '2.4rem',
+        medium: '3.2rem',
+        large: '4.0rem',
+        xlarge: '4.8rem',
+        xxlarge: '6.4rem'
+    }
+} as const
